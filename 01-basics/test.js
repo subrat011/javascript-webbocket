d942f59208b110f34d7cd40e182b73268acd1755
subrat011 <151999689+subrat011@users.noreply.github.com>
2023-11-28 10:19:50 +0000

    intruduced file saved

---
+++ 01-basics/test.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+/*
+1.javascript is a High level scripting language to use on developing web and mobile application efficiently.
+2.We used javascript on client side as well as server side.
+3.in javascript we do logical implementation in web pages.
+4.in today era we use Esb version of javascript ,as it is latest one.
+* ES6- Echma Script 6(vanilla javascript)
+
+*/
